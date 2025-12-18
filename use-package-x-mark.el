87eaa021a4de3900e60ee2661f54c6797403dc20
Elías Gabriel Pérez <eg642616@gmail.com>
2025-12-17 22:04:04 -0600

    Make :mark handle the selected packages property after init

--- use-package-x-mark.el
+++ use-package-x-mark.el
@@ -4,7 +4,7 @@
 
 ;; Author: Elias G. Perez <eg642616@gmail.com>
 ;; Keywords: convenience, tools, extensions
-;; Package-Requires: ((use-package "2.1"))
+;; Package-Requires: ((use-package "2.1") (package "1.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -38,20 +38,30 @@
 
 ;;; Requires
 (require 'use-package)
+(require 'package)
 
 
 
+;;; Variables
+(defvar use-package-x--marked-packages nil
+  "Used to set the packages in `package-selected-packages' after init.")
+
 ;;; Functions
+
+;; `package--save-selected-packages' can set
+;; `package-selected-packages' to nil, so ensure our packages are set.
+(defun use-package-x--mark-packages ()
+  (package--update-selected-packages
+   `(use-package-x ,@use-package-x--marked-packages) nil))
+(add-hook 'after-init-hook #'use-package-x--mark-packages)
 
 ;;;###autoload
 (defalias 'use-package-normalize/:mark 'use-package-normalize-predicate)
 
 ;;;###autoload
 (defun use-package-handler/:mark (name _keyword _args rest state)
-  (when (boundp 'package-selected-packages)
-    (add-to-list 'package-selected-packages 'use-package-x))
   (use-package-concat
-   `((add-to-list 'package-selected-packages ',name))
+   `((add-to-list 'use-package-x--marked-packages ',name))
    (use-package-process-keywords name rest state)))
 
 (provide 'use-package-x-mark)
