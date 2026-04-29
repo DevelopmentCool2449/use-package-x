2c7e4d301e6f0f0b129a037b49fb96515aeacb3d
Elias Gabriel Perez <eg642616@gmail.com>
2026-04-29 11:43:32 -0600

    Change license from GPL 3.0-or-later to Unlicense license

--- use-package-x-mark.el
+++ use-package-x-mark.el
@@ -6,18 +6,8 @@
 ;; Keywords: convenience, tools, extensions
 ;; Package-Requires: ((use-package "2.1") (package "1.1"))
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; This is free and unencumbered software released into the public domain.
+;; For more information, please refer to <https://unlicense.org/>
 
 ;;; Commentary:
 
